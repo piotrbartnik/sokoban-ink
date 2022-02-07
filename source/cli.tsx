0c1563494d56ce66a8889a0548785ca078d682c9
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-02-07 22:27:21 +0100

    chore: add initial dino in terminal repository

---
+++ source/cli.tsx
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './ui';
+
+const cli = meow(`
+	Usage
+	  $ dinoInTerminal
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ dinoInTerminal --name=Jane
+	  Hello, Jane
+`, {
+	flags: {
+		name: {
+			type: 'string'
+		}
+	}
+});
+
+render(<App name={cli.flags.name}/>);
