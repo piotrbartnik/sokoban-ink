f469830a5c410bd6cf5f90d7c0ed7e13e0a20d17
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-02-07 23:14:44 +0100

    chore: add commitlint with husky

--- source/ui.tsx
+++ source/ui.tsx
@@ -2,12 +2,9 @@ import React, { FC } from "react";
 import { Text } from "ink";
 
 const App: FC<{ name?: string }> = ({ name = "Stranger" }) => {
-	console.log("test");
-
 	return (
 		<Text>
 			Hello, <Text color="green">{name}</Text>
-			{console.log("test")}
 		</Text>
 	);
 };
