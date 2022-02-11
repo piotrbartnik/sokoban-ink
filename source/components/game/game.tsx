f9e4e0a5890c78fede7676b068040ba788a80d00
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-02-11 20:43:48 +0100

    chore: try diferent placements for player component

--- source/components/game/game.tsx
+++ source/components/game/game.tsx
@@ -5,12 +5,14 @@ import { Player } from "./player/player";
 export const Game: React.FC = () => {
 	return (
 		<Box flexDirection="column">
-			<Player positionX={5} positionY={5} />
+			<Player positionX={1} positionY={1} />
+
 			<Text>&#x1F916; &#x1F4E6; &#x1F538;</Text>
 			<Box borderStyle="bold" width={15} height={1} />
 			<Box>
 				<Box borderStyle="bold" width={3} height={5} />
 				<Box width={9} height={5} />
+
 				<Box borderStyle="bold" width={3} height={5} />
 			</Box>
 		</Box>
